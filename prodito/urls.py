bc958c374cd5ef43365697f321f9699d019d9522
PauloJacko <paulo.millanh@gmail.com>
2025-04-21 11:00:56 -0400

    INICIO DE SESION Y CREAR CUENTA LISTO

--- prodito/urls.py
+++ prodito/urls.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from django.urls import path
+from django.contrib.auth.views import LogoutView
 
 from . import views
 
@@ -10,6 +11,6 @@ urlpatterns = [
     path("login/google/", views.google_login, name="google_login"),
     path("oauth2callback/", views.oauth2callback, name="oauth2callback"),
     path("crear_cuenta/", views.crear_cuenta, name="crear_cuenta"),
-    path('inicio_sesion', views.inicio_sesion, name='inicio_sesion'),
-    
+    path('inicio_sesion/', views.inicio_sesion, name='inicio_sesion'),
+    path('cerrar_sesion/', LogoutView.as_view(next_page='bienvenida'), name='cerrar_sesion'),
 ]
