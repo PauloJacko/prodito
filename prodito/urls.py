0b7344446a64a04be2d90aa7d3837b1d727b1038
PauloJacko <paulo.millanh@gmail.com>
2025-04-21 11:00:14 -0400

    CREAR CUENTA E INICIO DE SESION LISTO!! Agregado boton de CERRAR SESION EN home

--- prodito/urls.py
+++ prodito/urls.py
@@ -10,4 +10,6 @@ urlpatterns = [
     path("login/google/", views.google_login, name="google_login"),
     path("oauth2callback/", views.oauth2callback, name="oauth2callback"),
     path("crear_cuenta/", views.crear_cuenta, name="crear_cuenta"),
+    path('inicio_sesion', views.inicio_sesion, name='inicio_sesion'),
+    
 ]
--- prodito/views.py
+++ prodito/views.py
@@ -2,9 +2,10 @@ from urllib.parse import urlencode
 
 import requests
 from django.conf import settings
-from django.contrib.auth import login
+from django.contrib.auth import authenticate, login
 from django.contrib.auth.models import User
 from django.shortcuts import redirect, render
+from django.contrib import messages
 
 
 def bienvenida(request):
@@ -16,7 +17,49 @@ def home(request):
 
 
 def crear_cuenta(request):
-    return render(request, "crear_cuenta.html")
+    if request.method == "POST":
+        username = request.POST['username']
+        email = request.POST['email']
+        password1 = request.POST['password1']
+        password2 = request.POST['password2']
+
+        if password1 != password2:
+            messages.error(request, "Las contraseñas no coinciden.")
+            return redirect('crear_cuenta')
+
+        if User.objects.filter(username=username).exists():
+            messages.error(request, "El nombre de usuario ya existe.")
+            return redirect('crear_cuenta')
+
+        if User.objects.filter(email=email).exists():
+            messages.error(request, "El correo ya está registrado.")
+            return redirect('crear_cuenta')
+
+        user = User.objects.create_user(username=username, email=email, password=password1)
+        user.save()
+        messages.success(request, "Cuenta creada exitosamente. Ahora puedes iniciar sesión.")
+        return redirect('bienvenida')
+
+    return render(request, 'crear_cuenta.html')
+
+def inicio_sesion(request):
+    if request.method == 'POST':
+        email = request.POST['email']
+        password = request.POST['password']
+
+        try:
+            username = User.objects.get(email=email).username
+        except User.DoesNotExist:
+            messages.error(request, "Correo no registrado.")
+            return redirect('bienvenida')
+
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.error(request, "Contraseña incorrecta.")
+            return redirect('bienvenida')
 
 
 def google_login(request):
